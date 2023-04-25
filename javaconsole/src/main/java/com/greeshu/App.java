be9080581df817675aea3a2b0461b6fca10572ab
Greeshma <greeshmagreeshu559@gmail.com>
2023-04-25 15:35:51 +0530

    maven projects are added

---
+++ javaconsole/src/main/java/com/greeshu/App.java
@@ -0,0 +1,13 @@
+package com.greeshu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Greeshu!,welcome to maven" );
+    }
+}
